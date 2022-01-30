9c75eb1413af25f03e1f6cde93c8a830c8ab736f
suvasis999 <talk2suvasis@gmail.com>
2022-01-30 18:52:22 +0530

    initial commit

---
+++ config/localhost.js
@@ -0,0 +1,6 @@
+
+export const localhost='http://localhost/cando/api/';
+export const imgUrlApi='http://localhost/cando/uploads/';
+
+//export const localhost='http://starprosolution.com/motogo/api/';
+//export const imgUrlApi='http://starprosolution.com/motogo/uploads/';
