dfac718f98e5425af4a8c4589e826c3ed2fb5088
suvasis999 <talk2suvasis@gmail.com>
2022-03-02 20:45:08 +0530

    'updates'

--- config/localhost.js
+++ config/localhost.js
@@ -2,8 +2,5 @@
 //export const localhost='http://localhost/cando/api/';
 //export const imgUrlApi='http://localhost/cando/uploads/';
 
-//export const localhost='http://starprosolution.com/motogo/api/';
-//export const imgUrlApi='http://starprosolution.com/motogo/uploads/';
-
 export const localhost='http://starprosolution.com/cando/api/';
 export const imgUrlApi='http://starprosolution.com/cando/uploads/';
