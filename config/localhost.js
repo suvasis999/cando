0ec015ef6cfb6be215cdbc0785b4726e23aff6d6
suvasis999 <talk2suvasis@gmail.com>
2022-03-06 17:24:20 +0530

    'updates'

--- config/localhost.js
+++ config/localhost.js
@@ -1,6 +1,6 @@
 
-export const localhost='http://localhost/cando/api/';
-export const imgUrlApi='http://localhost/cando/uploads/';
+//export const localhost='http://localhost/cando/api/';
+//export const imgUrlApi='http://localhost/cando/uploads/';
 
-//export const localhost='http://starprosolution.com/cando/api/';
-//export const imgUrlApi='http://starprosolution.com/cando/uploads/';
+export const localhost='http://starprosolution.com/cando/api/';
+export const imgUrlApi='http://starprosolution.com/cando/uploads/';
